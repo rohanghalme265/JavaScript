84154d311c95880618800c1ecc4335ba2205db67
Rohan Ghalme <ghalmerohan265@gmail.com>
2026-02-24 19:00:44 +0530

    impr:add question

--- Chapter_07_DOM-Part2/practice_01.js
+++ Chapter_07_DOM-Part2/practice_01.js
@@ -1,4 +1,27 @@
-console.log("hello");
+// console.log("hello");
+
+//// Q1). Create a new button element. Give it a text "click me",background color of red & text color of white .
+
+          //  Insert the button as the first element inside the body tag.
+
+
+let newbtn = document.createElement("button");
+newbtn.innerText = "Click me!";
+
+newbtn.style.color = "black";
+newbtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newbtn);
+// doc.prepend(newbut);
+
+
+
+
+
+
+
+
+
 
 
 
